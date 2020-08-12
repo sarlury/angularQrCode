1a82f94b00dfcce64cca45721bca452ccd538cba
sarlury <jemy.sarlury@gmail.com>
2020-08-12 07:43:35 +0700

    angular qrcode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgxQRCodeModule } from '@techiediaries/ngx-qrcode';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxQRCodeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
